4af8675da03e3104f28f76c89e8cbced45c1b8fc
almonzeir <monzeer2002@gmail.com>
2025-07-06 08:38:24 +0800

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer className="border-t border-border/40 py-8 text-center text-sm text-muted-foreground">
       <div className="container">
-        <p>&copy; {new Date().getFullYear()}  دليلك في ماليزيا. جميع الحقوق محفوظة المنذر @2025.</p>
+        <p>&copy; {new Date().getFullYear()}  دليلك في ماليزيا. جميع الحقوق محفوظة المنذر </p>
         <p className="mt-1">
         صُمم بحب بواسطة المنذر حامد لكل من يرغب بالدراسة في ماليزيا.
         </p>
